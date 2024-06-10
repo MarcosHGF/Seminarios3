b19437427e12104611eebe9a353c0d84ecd01d67
Marcos <marcosgollinfilho@gmail.com>
2024-06-10 11:50:26 -0300

    pagina inicial e alfabeto

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-document.getElementById('addTaskBtn').addEventListener('click', function() {
-    const taskInput = document.getElementById('taskInput');
-    const taskList = document.getElementById('taskList');
-    
-    if (taskInput.value.trim() !== "") {
-        const li = document.createElement('li');
-        li.textContent = taskInput.value;
-        
-        const deleteBtn = document.createElement('button');
-        deleteBtn.textContent = 'Remover';
-        deleteBtn.addEventListener('click', function() {
-            taskList.removeChild(li);
-        });
-        
-        li.appendChild(deleteBtn);
-        taskList.appendChild(li);
-        taskInput.value = '';
+function navigateTo(page) {
+    window.location.href = page;
+}
+
+function filterItems() {
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    const gridItems = document.getElementsByClassName('grid-item');
+
+    for (let i = 0; i < gridItems.length; i++) {
+        const itemText = gridItems[i].getElementsByTagName('span')[0].textContent.toLowerCase();
+        if (itemText.includes(searchInput)) {
+            gridItems[i].style.display = '';
+        } else {
+            gridItems[i].style.display = 'none';
+        }
     }
-});
+}
